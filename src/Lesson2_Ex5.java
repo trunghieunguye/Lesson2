38afb48d554c24f231a0d7a06c906e6aaa0dfc53
trunghieunguye <114744361+trunghieunguye@users.noreply.github.com>
2022-10-26 01:59:15 +0700

    okk

--- src/Lesson2_Ex5.java
+++ src/Lesson2_Ex5.java
@@ -1,2 +1,7 @@
 public class Lesson2_Ex5 {
+    public static void main(String args[]) {
+        String s1 = "Her name is Linh";
+        String replaceString = s1.replace("is", "was");//thay thế tất cả chuỗi "is" to "was"
+        System.out.println(replaceString);
+    }
 }
