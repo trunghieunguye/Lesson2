4c69b8e977b58c59658f0e1c5f1e1e75b63eed0e
trunghieunguye <114744361+trunghieunguye@users.noreply.github.com>
2022-10-26 10:50:24 +0700

    okk

--- src/Lesson2_Ex2.java
+++ src/Lesson2_Ex2.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.lang.String;
+
 public class Lesson2_Ex2 {
+    public static void main(String[] args) {
+
+        double a, b,c, x;
+        Scanner input = new Scanner(System.in);
+        System.out.print("a = ");
+        a = input.nextDouble();
+        System.out.print("b = ");
+        b = input.nextDouble();
+        System.out.print("c = ");
+        c = input.nextDouble();
+        String s = (a == 0) ? "Phương trình vô nghiệm" : "Phương trình có 2 nghiệm";
+        System.out.println(s);
+        double d = (b * b) - 4 * a * c; // d la delta
+        double x1 = ((-b) + Math.sqrt(d)) / (2 * a);
+        double x2 = ((-b) - Math.sqrt(d)) / (2 * a);
+        System.out.println("Gia tri cua nghiem thu nhat la " + x1);
+        System.out.println("Gia tri cua nghiem thu hai la " + x2);
+    }
 }
--- src/Lesson2_Ex4.java
+++ src/Lesson2_Ex4.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 public class Lesson2_Ex4 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập ngày sinh của bạn (dd/MM/yyyy): ");
+    String day = sc.nextLine();
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LocalDate birthDay = LocalDate.parse(day,format);
+    LocalDate currentDay = LocalDate.now();
+    Period age = Period.between(birthDay,currentDay);
+    String ketqua = birthDay.isAfter(currentDay)? "Bạn chưa sinh ra" : "Tuổi của bạn hiện tại: "+age.getYears();
+        System.out.print(ketqua);
+}
 }
